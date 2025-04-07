325362e20fe1910f75d81515d1d8eb1d298bc773
powerwarez <powerware@naver.com>
2025-04-07 21:55:09 +0900

    카카오 로그인으로 수정

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -27,12 +27,12 @@ export default function Login() {
     setIsLoading(false);
   }, []);
 
-  const handleGoogleLogin = async () => {
+  const handlekakaoLogin = async () => {
     if (!redirectUrl) return;
 
     setIsLoading(true);
     await supabase.auth.signInWithOAuth({
-      provider: "google",
+      provider: "kakao",
       options: {
         redirectTo: redirectUrl,
       },
@@ -55,7 +55,7 @@ export default function Login() {
           isClient && (
             <div className="flex flex-col space-y-4">
               <button
-                onClick={handleGoogleLogin}
+                onClick={handlekakaoLogin}
                 className="w-full bg-white border border-gray-300 text-gray-700 py-3 px-4 rounded-full font-semibold flex items-center justify-center shadow-sm hover:shadow-md transition-shadow"
               >
                 <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24">
@@ -76,7 +76,7 @@ export default function Login() {
                     d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
                   />
                 </svg>
-                <span>구글 계정으로 로그인</span>
+                <span>카카오계정으로 로그인</span>
               </button>
             </div>
           )
