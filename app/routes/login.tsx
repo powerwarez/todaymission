40085c291a9a8867d1f42829647fa9478ce4d09a
powerwarez <powerware@naver.com>
2025-04-07 21:57:31 +0900

    카카오로그인으로 수정

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -56,24 +56,14 @@ export default function Login() {
             <div className="flex flex-col space-y-4">
               <button
                 onClick={handlekakaoLogin}
-                className="w-full bg-white border border-gray-300 text-gray-700 py-3 px-4 rounded-full font-semibold flex items-center justify-center shadow-sm hover:shadow-md transition-shadow"
+                className="w-full bg-[#FEE500] border border-gray-300 text-[#3A1D1D] py-3 px-4 rounded-full font-semibold flex items-center justify-center shadow-sm hover:shadow-md transition-shadow"
               >
-                <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24">
+                <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="none">
                   <path
-                    fill="#4285F4"
-                    d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                  />
-                  <path
-                    fill="#34A853"
-                    d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                  />
-                  <path
-                    fill="#FBBC05"
-                    d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                  />
-                  <path
-                    fill="#EA4335"
-                    d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 4C7.58172 4 4 6.69624 4 10C4 11.8917 5.10154 13.5731 6.85125 14.6264L5.87045 18.2819C5.78958 18.5606 6.10792 18.7857 6.35526 18.6445L10.6139 16.1953C11.0661 16.2618 11.5275 16.2963 12 16.2963C16.4183 16.2963 20 13.6001 20 10.2963C20 6.99254 16.4183 4 12 4Z"
+                    fill="#3A1D1D"
                   />
                 </svg>
                 <span>카카오계정으로 로그인</span>
